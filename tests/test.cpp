0989d4c827b501c6411593bb0dadeac71bc0a659
chvikandr <vika_chil@mail.ru>
2022-05-30 18:31:24 +0300

    Первые тесты.проверка

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,2 +1,26 @@
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 #include "gmock/gmock.h"
+#include <Transaction.h>
+#include <Account.h>
+
+
+class MockAccount: public Account{
+public:
+    MockAccount(int id, int balance):Account(id, balance){}
+    MOCK_METHOD(void, Unlock, ());
+};
+TEST(Account, Init){
+    MockAccount test(1,100);
+    EXPECT_EQ(test.GetBalance(),100);
+    EXPECT_EQ(test.id(),1);
+}
+
+class MockTransaction: public Transaction{
+public:
+    MOCK_METHOD(void, SaveToDataBase, (Account& from, Account& to, int sum), (override));
+};
+
+TEST(Account, GetBalance){
+MockAccount acc(1,100);
+EXPECT_EQ(acc.Account::GetBalance(), 100);
+}
